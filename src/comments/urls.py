cf17660cbf18a3a71c045caefc105fd7730d5e72
FENIXYR <fenix.top.2006@gmail.com>
2024-12-17 15:35:21 +0300

    [feat] Update
    Signed-off-by: FENIXYR <fenix.top.2006@gmail.com>

--- src/comments/urls.py
+++ src/comments/urls.py
@@ -3,6 +3,6 @@ from django.urls import re_path, path
 from comments import views
 
 urlpatterns = [
-    re_path(r"^comments/<int:id>", views.put_delete_comments),
+    re_path(r"^comments/<uuid:id>", views.put_delete_comments),
     re_path(r'^comments', views.get_post_comments, name='comments_list'),
 ]
